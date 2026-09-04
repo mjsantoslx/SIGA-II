76c484a1d296d9c5e6e7f8e1bfa5c15adffcedc8
MJSantos <mjsantoslx@gmail.com>
2026-09-04 22:36:59 +0100

    V 01.22

--- siga/vendor/composer/installed.php
+++ siga/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'uep/siga',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'c44144afbb11a032cdb077666ef0bfe03e2988bc',
+        'reference' => 'd188af2cab8f4c12c554e6108098d728a41a19e7',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'uep/siga' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'c44144afbb11a032cdb077666ef0bfe03e2988bc',
+            'reference' => 'd188af2cab8f4c12c554e6108098d728a41a19e7',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
